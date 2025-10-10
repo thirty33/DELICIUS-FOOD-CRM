cda62d88b671909ea107d2fc18b302fca9f7b003
thirty33 <joelsuarez.1101@gmail.com>
2025-10-10 15:17:23 -0400

    fix error on categoyMenus api

--- app/Filters/Category/SortFilter.php
+++ app/Filters/Category/SortFilter.php
@@ -10,9 +10,9 @@ final class SortFilter extends Filter
     public function handle(Builder $items, \Closure $next): Builder
     {
         $filterData = $this->filter->getValue();
-        
+
         // Only apply default ordering if no category group ordering was applied
-        if (!$filterData || !isset($filterData['skip_default_sort'])) {
+        if (!$filterData || !isset($filterData['skip_default_sort']) || !$filterData['skip_default_sort']) {
             $items->orderBy('category_menu.display_order', 'asc');
         }
 
